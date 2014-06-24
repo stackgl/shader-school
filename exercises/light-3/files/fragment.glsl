6eaa0f662201dc2d919b53a8e5c7baa7084301ec
Mikola Lysenko <mikolalysenko@gmail.com>
2014-06-24 09:48:11 -0500

    adding extra uniforms to shaders

--- exercises/light-3/files/fragment.glsl
+++ exercises/light-3/files/fragment.glsl
@@ -2,7 +2,8 @@ precision highp float;
 
 uniform mat4 model, view, projection;
 uniform mat4 inverseModel, inverseView, inverseProjection;
-uniform vec3 ambient, diffuse, lightDirection;
+uniform vec3 ambient, diffuse, specular, lightDirection, eyeDirection;
+uniform float shininess;
 
 void main() {
   gl_FragColor = vec4(1,1,1,1);
--- exercises/light-3/files/vertex.glsl
+++ exercises/light-3/files/vertex.glsl
@@ -3,7 +3,8 @@ precision highp float;
 attribute vec4 position, normal;
 uniform mat4 model, view, projection;
 uniform mat4 inverseModel, inverseView, inverseProjection;
-uniform vec3 ambient, diffuse, lightDirection;
+uniform vec3 ambient, diffuse, specular, lightDirection, eyeDirection;
+uniform float shininess;
 
 void main() {
   gl_Position = vec4(0,0,0,1);
