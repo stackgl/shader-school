8a91636fe8363f7af57d956f63aa52d8d5865caa
Mikola Lysenko <mikolalysenko@gmail.com>
2014-06-25 20:06:03 -0500

    update template file

--- exercises/frag-2/files/fragment.glsl
+++ exercises/frag-2/files/fragment.glsl
@@ -2,13 +2,11 @@ precision highp float;
 
 uniform sampler2D texture;
 uniform vec2 screenSize;
-uniform vec2 center;
-uniform float theta;
 
 void main() {
   vec2 coord = gl_FragCoord.xy / screenSize;
 
-  //TODO: Transform coord by rotating about center
+  //TODO: Swap red and blue color channels of image
 
   gl_FragColor = texture2D(texture, coord);
 }
