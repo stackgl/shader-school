51e75d7558ee096c1a3f1198e88b326f8e25f6b4
Mikola Lysenko <mikolalysenko@gmail.com>
2015-03-08 14:42:01 -0700

    fix inconsistency in coordinate system for lighting

--- exercises/light-2/shaders/vertex.glsl
+++ exercises/light-2/shaders/vertex.glsl
@@ -6,7 +6,7 @@ varying vec3 fragNormal;
 
 void main() {
   vec4 worldPosition = model * vec4(position, 1.0);
-  vec4 worldNormal = vec4(normal, 0.0) * inverseModel;
+  vec4 worldNormal = vec4(normal, 0.0) * inverseModel * inverseView;
 
   gl_Position = projection * view * worldPosition;
   fragNormal = worldNormal.xyz;
